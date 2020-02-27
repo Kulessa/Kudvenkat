898520d80efb81454e0e841c9a400c339efb858a
Cristian Richard Kulessa <kulessa@gmail.com>
2020-02-27 13:50:24 -0300

    atividades uso de static mais eficiência no uso de memória

--- CSharpTutorial/Part020Activity04/Part020Activity04/Program.cs
+++ CSharpTutorial/Part020Activity04/Part020Activity04/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Part020Activity03
+namespace Part020Activity04
 {
     class Program
     {
@@ -9,9 +9,6 @@ namespace Part020Activity03
             Circle C1 = new Circle(5);
             float area1 = C1.CalculateArea();
             Console.WriteLine("Area = {0}", area1);
-            
-            //C1.Print();
-            Circle.Print();
 
             Circle C2 = new Circle(6);
             float area2 = C2.CalculateArea();
@@ -20,20 +17,28 @@ namespace Part020Activity03
     }
     class Circle
     {
-        static float pi = 3.141F;
+        //3
+        // /*1*/static float pi = 3.141F;
+        static float pi;
         int radius;
 
+        //3
+        static Circle()
+        {
+            Console.WriteLine("Static Constructor Called");
+            Circle.pi = 3.141F;
+        }
+        
+        //2
+        //Circle(int radius)
         public Circle(int radius)
         {
+            Console.WriteLine("Instace Constructor Called");
             this.radius = radius;
         }
         public float CalculateArea()
         {
             return Circle.pi * this.radius * this.radius;
-        }
-        public static void Print()
-        {
-            //
         }
     }
 
---
+++ CSharpTutorial/Part020Activity05/Part020Activity05/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Part020Activity05
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(Circle.pi);
+        }
+    }
+    class Circle
+    {
+        public static float pi;
+        int radius;
+
+        static Circle()
+        {
+            Console.WriteLine("Static Constructor Called");
+            Circle.pi = 3.141F;
+        }
+
+        public Circle(int radius)
+        {
+            Console.WriteLine("Instace Constructor Called");
+            this.radius = radius;
+        }
+        public float CalculateArea()
+        {
+            return Circle.pi * this.radius * this.radius;
+        }
+    }
+}
