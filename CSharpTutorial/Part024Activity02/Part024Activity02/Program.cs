72cd9deb562e262394bd0047e4a7a7ff5c4b3d6d
Cristian Richard Kulessa <kulessa@gmail.com>
2020-03-07 23:55:50 -0300

    Polymorphism

---
+++ CSharpTutorial/Part024Activity02/Part024Activity02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part024Activity02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass b = new DerivedClass();
+            b.Print();
+        }
+    }
+    public class BaseClass
+    {
+        public virtual void Print()
+        {
+            Console.WriteLine("I am a Base Class Print Method");
+        }
+    }
+    public class DerivedClass : BaseClass
+    {
+        public new void Print()
+        {
+            Console.WriteLine("I am a Derived Class Print Method");
+        }
+    }
+}
---
+++ CSharpTutorial/Part024Activity03/Part024Activity03/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part024Activity03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass b = new DerivedClass();
+            b.Print();
+
+            DerivedClass d = new DerivedClass();
+            d.Print();
+        }
+    }
+    public class BaseClass
+    {
+        public virtual void Print()
+        {
+            Console.WriteLine("I am a Base Class Print Method");
+        }
+    }
+    public class DerivedClass : BaseClass
+    {
+        public new void Print()
+        {
+            Console.WriteLine("I am a Derived Class Print Method");
+        }
+    }
+}
